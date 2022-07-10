31308443446a4890aca4e616b4d4568303f55015
igorkll <69525332+igorkll@users.noreply.github.com>
2022-07-10 14:36:22 +0300

    update

--- system/calls/system_setTheme.lua
+++ system/calls/system_setTheme.lua
@@ -2,5 +2,7 @@ local fs = require("filesystem")
 local calls = require("calls")
 local path = ...
 
-fs.copy(path, "/data/theme.plt")
+if path ~= "/data/theme.plt" then
+    fs.copy(path, "/data/theme.plt")
+end
 calls.call("system_applyTheme", path)
--- system/lib/gui_container.lua
+++ system/lib/gui_container.lua
@@ -5,47 +5,22 @@ local calls = require("calls")
 
 local gui_container = {}
 
-gui_container.screen = "20108ef5-444e-46bc-bd6c-48aee518009e"
+gui_container.colors = {}
+gui_container.indexsColors = {}
 
-gui_container.colors = { --computercraft colors
-    white     = 0xF0F0F0,
-    orange    = 0xF2B233,
-    magenta   = 0xE57FD8,
-    lightBlue = 0x99B2F2,
-    yellow    = 0xDEDE6C,
-    lime      = 0x7FCC19,
-    pink      = 0xF2B2CC,
-    gray      = 0x4C4C4C,
-    lightGray = 0x999999,
-    cyan      = 0x4C99B2,
-    purple    = 0xB266E5,
-    blue      = 0x3366CC,
-    brown     = 0x7F664C,
-    green     = 0x57A64E,
-    red       = 0xCC4C4C,
-    black     = 0x191919
-}
+local function attempt(path)
+    if fs.exists(path) then
+        calls.call("system_applyTheme", path)
+        return false
+    end
+    return true
+end
 
-gui_container.indexsColors = { --computercraft colors
-    0xF0F0F0, --1)  white
-    0xF2B233, --2)  orange
-    0xE57FD8, --3)  magenta
-    0x99B2F2, --4)  lightBlue
-    0xDEDE6C, --5)  yellow
-    0x7FCC19, --6)  lime
-    0xF2B2CC, --7)  pink
-    0x4C4C4C, --8)  gray
-    0x999999, --9)  lightGray
-    0x4C99B2, --10) cyan
-    0xB266E5, --11) purple
-    0x3366CC, --12) blue
-    0x7F664C, --13) brown
-    0x57A64E, --14) green
-    0xCC4C4C, --15) red
-    0x191919  --16) black
-}
 if fs.exists("/data/theme.plt") then
     calls.call("system_applyTheme", "/data/theme.plt")
+else
+    pcall(fs.copy, "/system/themes/classic.plt", "/data/theme.plt")
+    calls.call("system_applyTheme", "/system/themes/classic.plt")
 end
 
 return gui_container
