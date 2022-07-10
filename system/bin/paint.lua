b2d64f1609a1e8b969731880297a801c85815319
igorkll <69525332+igorkll@users.noreply.github.com>
2022-07-10 20:25:04 +0300

    Update paint.lua

--- system/bin/paint.lua
+++ system/bin/paint.lua
@@ -60,12 +60,54 @@ end
 draw()
 
 local function load()
-    local file = assert(fs.open(filepath, "rb"))
-    local sizeX = string.byte(file.read(1))
-    local sizeY = string.byte(file.read(1))
-    file.seek("cur", 8)
+    local readbit = calls.load("readbit")
 
-    
+    local file = assert(fs.open(filepath, "rb"))
+    local buffer = file.readAll()
+    file.close()
+    local function read(bytecount)
+        local str = buffer:sub(1, bytecount)
+        buffer = buffer:sub(bytecount + 1, #buffer)
+        return str
+    end
+
+    local sizeX = string.byte(read(1))
+    local sizeY = string.byte(read(1))
+    read(8)
+
+    local colorByte, countCharBytes, background, foreground, char
+    for cy = 1, sizeY do
+        for cx = 1, sizeX do
+            colorByte      = string.byte(read(1))
+            countCharBytes = string.byte(read(1))
+
+            background = 
+            ((readbit(colorByte, 1) and 1 or 0) * 1) + 
+            ((readbit(colorByte, 2) and 1 or 0) * 2) + 
+            ((readbit(colorByte, 3) and 1 or 0) * 4) + 
+            ((readbit(colorByte, 4) and 1 or 0) * 8)
+            foreground = 
+            ((readbit(colorByte, 5) and 1 or 0) * 1) + 
+            ((readbit(colorByte, 6) and 1 or 0) * 2) + 
+            ((readbit(colorByte, 7) and 1 or 0) * 4) + 
+            ((readbit(colorByte, 8) and 1 or 0) * 8)
+            background = colors[background]
+            foreground = colors[foreground]
+
+            if background ~= 0 and foreground ~= 0 then
+                char = read(countCharBytes)
+                if background ~= oldbackground then
+                    gpu.setBackground(background)
+                    oldbackground = background
+                end
+                if foreground ~= oldforeground then
+                    gpu.setForeground(foreground)
+                    oldforeground = foreground
+                end
+                gpu.set(cx + (x - 1), cy + (y - 1), char)
+            end
+        end
+    end
 end
 load()
 
