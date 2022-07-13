d11dfbd7315cbb082c56a3b053ba9b3a4976c212
igorkll <69525332+igorkll@users.noreply.github.com>
2022-07-13 16:27:36 +0300

    Update a_mounter.lua

--- system/autoruns/a_mounter.lua
+++ system/autoruns/a_mounter.lua
@@ -5,23 +5,23 @@ local event = require("event")
 local function genName()
     local number = 1
     while true do
-        if fs.exists("disk" .. tostring(number)) then
+        if fs.exists(paths.concat("/data/userdata", "disk" .. tostring(number))) then
             number = number + 1
         else
             break
         end
     end
-    return "disk" .. tostring(number)
+    return paths.concat("/data/userdata", "disk" .. tostring(number))
 end
 
 event.listen("component_added", function(_, uuid, name)
     if name == "filesystem" then
-        fs.mount(uuid, paths.concat("/data/userdata", genName()))
+        fs.mount(uuid, genName())
         event.push("redrawDesktop")
     end
 end)
 
-event.listen("component_added", function(_, uuid, name)
+event.listen("component_removed", function(_, uuid, name)
     if name == "filesystem" then
         for _, tbl in ipairs(fs.mountList) do
             if tbl[1].address == uuid then
