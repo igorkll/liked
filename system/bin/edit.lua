2e75addbb45564303d62807be741e779c648373c
igorkll <69525332+igorkll@users.noreply.github.com>
2022-07-14 15:00:44 +0300

    Update edit.lua

--- system/bin/edit.lua
+++ system/bin/edit.lua
@@ -1,8 +1,18 @@
 local fs = require("filesystem")
 local unicode = require("unicode")
 local calls = require("calls")
+local graphic = require("graphic")
+local calls = require("calls")
+local gui_container = require("gui_container")
 
 local screen, path = ...
+
+local colors = gui_container.colors
+local rx, ry
+do
+    local gpu = graphic.findGpu()
+    rx, ry = gpu.getResolution()
+end
 
 ------------------------------------
 
@@ -25,3 +35,19 @@ end
 
 ------------------------------------
 
+local offsetX = 0
+local offsetY = 0
+
+local function redraw()
+    local gpu = graphic.findGpu(screen)
+    gpu.setForeground(colors.black)
+    gpu.setBackground(colors.white)
+    gpu.fill(1, 1, rx, ry, " ")
+    for cy = 1, ry do
+        local line = lines[cy + offsetY]
+        if line then
+            gpu.set(1 + offsetX, cy, line)
+        end
+    end
+end
+redraw()
