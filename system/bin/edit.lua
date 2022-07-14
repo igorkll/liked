80ccbccbcb424ffedac1725e8152953a6d24e320
igorkll <69525332+igorkll@users.noreply.github.com>
2022-07-14 14:49:12 +0300

    Update edit.lua

--- system/bin/edit.lua
+++ system/bin/edit.lua
@@ -1,5 +1,6 @@
 local fs = require("filesystem")
 local unicode = require("unicode")
+local calls = require("calls")
 
 local screen, path = ...
 
@@ -7,10 +8,20 @@ local screen, path = ...
 
 local lines = {}
 
-local function save()
-    
+local function saveFile()
+    local file = fs.open(path, "w")
+    for i, v in ipairs(lines) do
+        file.write(v .. "\n")
+    end
+    file.close()
 end
 
-local function load()
-    
+local function loadFile()
+    local file = fs.open(path, "r")
+    local data = file.readAll()
+    file.close()
+    lines = calls.call("split", data, "\n")
 end
+
+------------------------------------
+
