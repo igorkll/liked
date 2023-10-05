c33d8f8a9b0c4c58182853b8327363a09bb32c5f
igorkll <frerther@gmail.com>
2023-10-05 20:43:14 +0300

    Update w_eeprom.lua

--- system/bin/settings.app/modules/w_eeprom.lua
+++ system/bin/settings.app/modules/w_eeprom.lua
@@ -16,77 +16,93 @@ local window = graphic.createWindow(screen, posX, posY, rx - (posX - 1), ry - (p
 
 local layout = uix.create(window)
 
-local checksumLabel = layout:createText(2, 2, colors.white)
+local codeSizeLabel = layout:createText(2, 2, colors.white)
+local dataSizeLabel = layout:createText(2, 3, colors.white)
+local maxCodeSizeLabel = layout:createText(18, 2, colors.white)
+local maxDataSizeLabel = layout:createText(18, 3, colors.white)
+local checksumLabel = layout:createText(2, 4, colors.white)
+local addrLabel = layout:createText(2, 5, colors.white)
 
-local flashButton = layout:createButton(2, 4, 16, 1, colors.white, colors.gray, "Flash")
-local dumpButton = layout:createButton(2, 6, 16, 1, colors.white, colors.gray, "Dump")
-local makeReadOnlyButton = layout:createButton(20, 4, 16, 1, colors.white, colors.gray, "Make R/O")
+local flashButton = layout:createButton(2, 7, 16, 1, colors.white, colors.gray, "Flash")
+local dumpButton = layout:createButton(2, 9, 16, 1, colors.white, colors.gray, "Dump")
+local makeReadOnlyButton = layout:createButton(20, 7, 16, 1, colors.white, colors.gray, "Make R/O")
 
 function flashButton:onClick()
-    os.sleep(0.1)
-    self.state = false
-    self:draw()
-    graphic.forceUpdate()
-    
-    if gui.pleaseCharge(screen, 20, "flash") then
-        local path = gui_filepicker(screen, nil, nil, nil, "lua", false, false)
-        if path then
-            local maxSize = math.round(component.eeprom.getSize())
-            local data = fs.readFile(path)
-            local fsize = #data
-            if fsize > maxSize then
-                gui.warn(screen, nil, nil, "it is not possible to write a " .. fsize .. " bytes file to an EEPROM with a capacity of " .. maxSize .. " bytes")
-            elseif gui.pleaseType(screen, "FLASH", "flash eeprom") then
-                gui_status(screen, nil, nil, "flashing...")
-                local result = {pcall(component.eeprom.set, data)}
-                if not result[1] then
-                    gui.warn(screen, nil, nil, tostring(result[2] or "unknown error"))
-                elseif result[3] then
-                    gui.warn(screen, nil, nil, tostring(result[3] or "unknown error"))
+    if component.eeprom then
+        os.sleep(0.1)
+        self.state = false
+        self:draw()
+        graphic.forceUpdate()
+        
+        if gui.pleaseCharge(screen, 20, "flash") then
+            local path = gui_filepicker(screen, nil, nil, nil, "lua", false, false)
+            if path then
+                local maxSize = math.round(component.eeprom.getSize())
+                local data = fs.readFile(path)
+                local fsize = #data
+                if fsize > maxSize then
+                    gui.warn(screen, nil, nil, "it is not possible to write a " .. fsize .. " bytes file to an EEPROM with a capacity of " .. maxSize .. " bytes")
+                elseif gui.pleaseType(screen, "FLASH", "flash eeprom") then
+                    gui_status(screen, nil, nil, "flashing...")
+                    local result = {pcall(component.eeprom.set, data)}
+                    if not result[1] then
+                        gui.warn(screen, nil, nil, tostring(result[2] or "unknown error"))
+                    elseif result[3] then
+                        gui.warn(screen, nil, nil, tostring(result[3] or "unknown error"))
+                    end
                 end
             end
         end
-    end
 
-    gRedraw()
-    redraw()
+        gRedraw()
+        redraw()
+    end
 end
 
 function dumpButton:onClick()
-    os.sleep(0.1)
-    self.state = false
-    self:draw()
-    graphic.forceUpdate()
+    if component.eeprom then
+        os.sleep(0.1)
+        self.state = false
+        self:draw()
+        graphic.forceUpdate()
 
-    local path = gui_filepicker(screen, nil, nil, nil, "lua", true, false)
-    if path then
-        local data = component.eeprom.get()
-        liked.assert(screen, fs.writeFile(path, data))
+        local path = gui_filepicker(screen, nil, nil, nil, "lua", true, false)
+        if path then
+            local data = component.eeprom.get()
+            liked.assert(screen, fs.writeFile(path, data))
+        end
+
+        gRedraw()
+        redraw()
     end
-
-    gRedraw()
-    redraw()
 end
 
 function makeReadOnlyButton:onClick()
-    os.sleep(0.1)
-    self.state = false
-    self:draw()
-    graphic.forceUpdate()
+    if component.eeprom then
+        os.sleep(0.1)
+        self.state = false
+        self:draw()
+        graphic.forceUpdate()
 
-    if gui.pleaseCharge(screen, 20, "readonly") and gui.pleaseType(screen, "READONLY", "make readonly") then
-        pcall(component.eeprom.makeReadonly, component.eeprom.getChecksum())
+        if gui.pleaseCharge(screen, 20, "readonly") and gui.pleaseType(screen, "READONLY", "make readonly") then
+            pcall(component.eeprom.makeReadonly, component.eeprom.getChecksum())
+        end
+
+        gRedraw()
+        redraw()
     end
-
-    gRedraw()
-    redraw()
 end
 
 ------------------------------------
 
 function redraw()
     window:clear(colors.black)
-    checksumLabel.text = "checksum: " .. tostring(component.eeprom.getChecksum())
+    codeSizeLabel.text = "code size: " .. math.round(#component.eeprom.get())
+    dataSizeLabel.text = "data size: " .. math.round(#component.eeprom.getData())
+    maxCodeSizeLabel.text = "/ " .. math.round(tonumber(component.eeprom.getSize()) or 0)
+    maxDataSizeLabel.text = "/ " .. math.round(tonumber(component.eeprom.getDataSize()) or 0)
+    checksumLabel.text = "checksum : " .. tostring(component.eeprom.getChecksum())
+    addrLabel.text     = "address  : " .. component.eeprom.address
     layout:draw()
 end
 redraw()
@@ -95,13 +111,7 @@ return function(eventData)
     local windowEventData = window:uploadEvent(eventData)
     layout:uploadEvent(windowEventData)
 
-    if windowEventData[1] == "touch" then
-        if windowEventData[3] >= 2 and windowEventData[3] <= 19 then
-            if windowEventData[4] == 2 then
-                --wipeUserData()
-            elseif windowEventData[4] == 4 then
-                --updateSystem()
-            end
-        end
+    if (eventData[1] == "component_added" or eventData[1] == "component_removed") and eventData[3] == "eeprom" then
+        redraw()
     end
 end
