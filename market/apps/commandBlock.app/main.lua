e1061dddc6dc67a569d7362fdecb3462693e4733
igorkll <frerther@gmail.com>
2023-10-08 17:07:14 +0300

    update

---
+++ market/apps/commandBlock.app/main.lua
@@ -0,0 +1,10 @@
+local graphic = require("graphic")
+local uix = require("uix")
+local event = require("event")
+local liked = require("liked")
+local gui = require("gui")
+local component = require("component")
+local colors = require("gui_container").colors
+local colorlib = require("colors")
+
+local screen = ...
