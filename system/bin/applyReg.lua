b352b3f92d5189cc4d0bc8bcc79aa8ddd29ecffa
igorkll <frerther@gmail.com>
2023-10-11 11:58:52 +0300

    Update applyReg.lua

--- system/bin/applyReg.lua
+++ system/bin/applyReg.lua
@@ -1,7 +1,11 @@
 local registry = require("registry")
 local liked = require("liked")
 local gui = require("gui")
+local gui_container = require("gui_container")
 local screen, _, path = ...
 
-gui.status(screen, nil, nil, "installing reg file \"" .. gui_container.toUserPath(screen, path) .. "\"")
-liked.assert(screen, registry.apple(path))
+local upath = gui_container.toUserPath(screen, path)
+if gui_yesno(screen, nil, nil, "do you really want to apply the " .. upath .. " registry modifier?") then
+    gui.status(screen, nil, nil, "installing reg file \"" .. upath .. "\"")
+    liked.assert(screen, registry.apply(path))
+end
