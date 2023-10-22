bcb1c368e87edb6cc4dde04e1d2c2db3b3251994
igorkll <frerther@gmail.com>
2023-10-22 16:51:11 +0300

    hotfix

--- system/bin/applyReg.lua
+++ system/bin/applyReg.lua
@@ -9,5 +9,6 @@ if force or gui_yesno(screen, nil, nil, "do you really want to apply the \"" .. 
     gui.status(screen, nil, nil, "installing reg file \"" .. gui_container.toUserPath(screen, path) .. "\"")
     if liked.assert(screen, registry.apply(path)) then
         registry.hotReload()
+        gui_container.refresh()
     end
 end
