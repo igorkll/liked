35e8188816da416e0d2a0297ea1769ce22f90a86
igorkll <frerther@gmail.com>
2023-11-08 22:58:06 +0300

    Update main.lua

--- market/apps/cardwriter.app/main.lua
+++ market/apps/cardwriter.app/main.lua
@@ -1,18 +1,24 @@
 local graphic = require("graphic")
 local uix = require("uix")
+local gui = require("gui")
+local colorlib = require("colors")
 
 local screen = ...
 local guimanager = {}
+local rx, ry = graphic.getResolution(screen)
 local layout = uix.createAuto(screen, "Card Writer")
 
+local offset = (rx / 2) - 11
+
 local score = 1
-local lblScore = layout:createLabel(10, 3, 5, 1)
+local lblScore = layout:createLabel(10 + offset, 3, 5, 1)
 function lblScore.update()
     lblScore.text = tostring(score)
     lblScore:draw()
 end
+lblScore.update()
 
-local subScore = layout:createButton(6, 3, 3, 1, nil, nil, "-")
+local subScore = layout:createButton(6 + offset, 3, 3, 1, nil, nil, "-")
 function subScore:onClick()
     score = score - 1
     if score < 1 then
@@ -22,7 +28,7 @@ function subScore:onClick()
     end
 end
 
-local subScore2 = layout:createButton(2, 3, 4, 1, nil, nil, "--")
+local subScore2 = layout:createButton(2 + offset, 3, 4, 1, nil, nil, "--")
 function subScore2:onClick()
     score = score - 10
     if score < 1 then
@@ -31,16 +37,25 @@ function subScore2:onClick()
     lblScore.update()
 end
 
-local addScore = layout:createButton(16, 3, 3, 1, nil, nil, "+")
+local addScore = layout:createButton(16 + offset, 3, 3, 1, nil, nil, "+")
 function addScore:onClick()
     score = score + 1
     lblScore.update()
 end
 
-local addScore2 = layout:createButton(16 + 3, 3, 4, 1, nil, nil, "++")
+local addScore2 = layout:createButton(16 + 3 + offset, 3, 4, 1, nil, nil, "++")
 function addScore2:onClick()
     score = score + 10
     lblScore.update()
+end
+
+local color = colorlib.white
+local selectColor = layout:createButton(offset + 3, 1, 16, 1, uix.colors.white, nil, "Select Color")
+function selectColor:onClick()
+    color = gui.selectcolor(screen)
+    uix.doColor(colorlib[color])
+    selectColor.fore2 = selectColor.back
+    selectColor.back2 = selectColor.fore
 end
 
 uix.loop(guimanager, layout)
