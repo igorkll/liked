d14e3ad4ca2a0a5d0242362dd881954ff4afa8a6
igorkll <frerther@gmail.com>
2023-11-08 23:10:37 +0300

    Update main.lua

--- market/apps/cardwriter.app/main.lua
+++ market/apps/cardwriter.app/main.lua
@@ -8,17 +8,15 @@ local guimanager = {}
 local rx, ry = graphic.getResolution(screen)
 local layout = uix.createAuto(screen, "Card Writer")
 
-local offset = (rx / 2) - 11
-
 local score = 1
-local lblScore = layout:createLabel(10 + offset, 3, 5, 1)
+local lblScore = layout:createLabel(10, 3, 5, 1)
 function lblScore.update()
     lblScore.text = tostring(score)
     lblScore:draw()
 end
 lblScore.update()
 
-local subScore = layout:createButton(6 + offset, 3, 3, 1, nil, nil, "-")
+local subScore = layout:createButton(6, 3, 3, 1, nil, nil, "-")
 function subScore:onClick()
     score = score - 1
     if score < 1 then
@@ -28,7 +26,7 @@ function subScore:onClick()
     end
 end
 
-local subScore2 = layout:createButton(2 + offset, 3, 4, 1, nil, nil, "--")
+local subScore2 = layout:createButton(2, 3, 4, 1, nil, nil, "--")
 function subScore2:onClick()
     score = score - 10
     if score < 1 then
@@ -37,25 +35,34 @@ function subScore2:onClick()
     lblScore.update()
 end
 
-local addScore = layout:createButton(16 + offset, 3, 3, 1, nil, nil, "+")
+local addScore = layout:createButton(16, 3, 3, 1, nil, nil, "+")
 function addScore:onClick()
     score = score + 1
     lblScore.update()
 end
 
-local addScore2 = layout:createButton(16 + 3 + offset, 3, 4, 1, nil, nil, "++")
+local addScore2 = layout:createButton(16 + 3, 3, 4, 1, nil, nil, "++")
 function addScore2:onClick()
     score = score + 10
     lblScore.update()
 end
 
+layout:createText(2, 7, nil, "make readonly:")
+local readonly = layout:createSwitch(17, 7, false, uix.colors.red)
+
 local color = colorlib.white
-local selectColor = layout:createButton(offset + 3, 1, 16, 1, uix.colors.white, nil, "Select Color")
+local selectColor = layout:createButton(2, 5, 16, 1, uix.colors.white, nil, "Select Color")
 function selectColor:onClick()
-    color = gui.selectcolor(screen)
-    uix.doColor(colorlib[color])
-    selectColor.fore2 = selectColor.back
-    selectColor.back2 = selectColor.fore
+    local newcolor = gui.selectcolor(screen)
+    if newcolor then
+        color = newcolor
+        uix.doColor(selectColor, uix.colors[colorlib[color]])
+        selectColor.fore2 = selectColor.back
+        selectColor.back2 = selectColor.fore
+    end
+    layout:draw()
 end
+
+
 
 uix.loop(guimanager, layout)
