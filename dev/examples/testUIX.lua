5f2befd4f6b3aaef49101cdbb0557d2525d5293e
igorkll <frerther@gmail.com>
2023-11-15 18:03:51 +0300

    Update testUIX.lua

--- dev/examples/testUIX.lua
+++ dev/examples/testUIX.lua
@@ -4,8 +4,17 @@ local ui = uix.manager(...)
 local layout1 = ui:create()
 local layout2 = ui:create("LOLZ")
 
+local toLayout2 = layout1:createButton(2, 2, 16, 3)
+function toLayout2:onClick()
+    ui:select(layout2)
+end
+
+local toLayout1 = layout2:createButton(2, 2, 16, 3)
+function toLayout1:onClick()
+    ui:select(layout1)
+end
+
 function ui:onExit()
     require("computer").beep()
 end
-
 ui:loop()
