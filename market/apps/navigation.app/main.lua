48996231dbd95b01c0241ab882e47f6433dc65f2
igorkll <frerther@gmail.com>
2023-11-22 22:16:05 +0300

    Update main.lua

--- market/apps/navigation.app/main.lua
+++ market/apps/navigation.app/main.lua
@@ -3,4 +3,10 @@ local gui = require("gui")
 
 local screen = ...
 local navigation = gui.selectcomponentProxy(screen, nil, nil, "navigation", true)
+local manager = uix.manager(screen)
+local layout = manager:create("Navigation", uix.colors.white)
 
+local canvas = layout:createCanvas(15, 1, 48, 24, uix.colors.white)
+
+
+manager:loop()
