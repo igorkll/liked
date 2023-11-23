91ff16e1377921be2664057c287254f06b3eb255
igorkll <frerther@gmail.com>
2023-11-23 20:18:53 +0300

    openbox refactor

--- market/apps/openbox.app/main.lua
+++ market/apps/openbox.app/main.lua
@@ -14,27 +14,4 @@ if not program then
     return
 end
 
-local box = openbox.create(screen)
-if box.term then
-    box.term:clear()
-end
-
-local ok, err = box:execute(assert(fs.readFile(program)))
-
-if box.screen then
-    local gpu = graphic.findGpu(box.screen)
-    gpu.setResolution(box.oldRX, box.oldRY)
-end
-
-if not ok then
-    local clear = saveBigZone(screen)
-    gui.bigWarn(screen, nil, nil, tostring(err))
-    clear()
-end
-
-if ok then
-    box.term:print("press enter key to exit the emulator...")
-    event.pull("key_down", lastinfo.keyboards[screen][1], 13, 28)
-end
-
-box:clear()
+openbox.run(screen, program)
