0fc7815f56ed3d98ebbd9692b13e3f7711660e66
igorkll <frerther@gmail.com>
2023-11-24 14:00:18 +0300

    tquest > TQueST

--- market/list.lua
+++ market/list.lua
@@ -381,15 +381,15 @@ local list = {
         urlPrimaryPart = selfurlpart .. "/apps/navigation.app/"
     },
     {
-        name = "tquest",
+        name = "TQueST",
         version = "1",
         vendor = "MineCR",
         description = "quest ported from hipOS.\ndeveloped by MineCR.\nyoutube - Max Play`n",
         minDiskSpace = 64,
         libs = {"openbox"},
 
-        path = "/data/apps/tquest.app",
-        urlPrimaryPart = selfurlpart .. "/apps/tquest.app/",
+        path = "/data/apps/TQueST.app",
+        urlPrimaryPart = selfurlpart .. "/apps/TQueST.app/",
         files = {"main.lua", "icon.t2p", "program.lua"}
     }
 }
