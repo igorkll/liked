f08caf740953087010803ae91de8cb9178ad6eb0
igorkll <frerther@gmail.com>
2023-11-24 17:38:11 +0300

    Update bmp.lua

--- market/libs/bmp.lua
+++ market/libs/bmp.lua
@@ -17,11 +17,21 @@ local function get16bit(buffer)
     return buffer[1] | buffer[2] << 8
 end
 
-function bmp.parse(path, sizecallback, callback)
+local function getBits(buffer)
+    local number = 0
+    local bitOffset = 0
+    for i = 1, #buffer do
+        number = number | (buffer[i] << bitOffset)
+        bitOffset = bitOffset + 8
+    end
+    return number
+end
+
+function bmp.parse(path, sizeCallback, callback)
     local file = fs.open(path, "rb")
 
     if file.read(2) ~= "BM" then
-        return nil, "not a BMP"
+        error("not a BMP", 2)
     end
 
     local filesize = get32bit(strToArray(file.read(4)))
@@ -29,12 +39,46 @@ function bmp.parse(path, sizecallback, callback)
     local dataOffset = get32bit(strToArray(file.read(4)))
 
     -- DIB HEADER
-
     local dibHeaderSize = get32bit(strToArray(file.read(4)))
     local width = get32bit(strToArray(file.read(4)))
     local height = get32bit(strToArray(file.read(4)))
-    local colourPlanes = get16bit(strToArray(file.read(2)))
+    local colorPlanes = get16bit(strToArray(file.read(2)))
     local bitsPerPixel = get16bit(strToArray(file.read(2)))
+    local compressionType = get32bit(strToArray(file.read(4)))
+    local imageSize = get32bit(strToArray(file.read(4)))
+    file.read(4)
+    file.read(4)
+    local numOfColors = get32bit(strToArray(file.read(4)))
+    local numOfImportantColors = get32bit(strToArray(file.read(4)))
+
+    -- Colors Profile
+    local redBitMask = get32bit(strToArray(file.read(4)))
+    local greenBitMask = get32bit(strToArray(file.read(4)))
+    local blueBitMask = get32bit(strToArray(file.read(4)))
+    local alphaBitMask = get32bit(strToArray(file.read(4)))
+
+    local colorSpace = get32bit(strToArray(file.read(4)))
+    file.read(4 * 4 * 2)
+
+    local gammaRed = get32bit(strToArray(file.read(4)))
+    local gammaGreen = get32bit(strToArray(file.read(4)))
+    local gammaBlue = get32bit(strToArray(file.read(4)))
+
+    local intent = get32bit(strToArray(file.read(4)))
+    local profileDataOffset = get32bit(strToArray(file.read(4)))
+    local profileDataSize = get32bit(strToArray(file.read(4)))
+
+    file.read(4)
+
+    -- parsing
+    sizeCallback(width, height)
+    for ix = 1, width + 1 do
+        for iy = height + 1, 1, -1 do
+            callback(ix, iy, getBits(file.read(bitsPerPixel)))
+        end
+    end
+
+    local currentOffset = dataOffset
 end
 
 return bmp
