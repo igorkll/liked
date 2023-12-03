7ddc8e05cb130f0a75b6932bcf5f587f9873fccd
igorkll <frerther@gmail.com>
2023-12-03 20:00:32 +0300

    Update palette.lua

--- system/lib/palette.lua
+++ system/lib/palette.lua
@@ -5,29 +5,17 @@ local sysinit = require("sysinit")
 local palette = {}
 
 function palette.save(screen)
-    local pal = {}
-    for i = 0, 15 do
-        pal[i] = graphic.getPaletteColor(screen, i)
-    end
-    return pal
+    return graphic.getPalette(screen, true)
 end
 
 function palette.set(screen, pal)
-    for i = 0, 15 do
-        if graphic.getPaletteColor(screen, i) ~= pal[i] then
-            graphic.setPaletteColor(screen, i, pal[i])
-        end
-    end
+    graphic.setPalette(screen, pal, true)
 end
 
 function palette.fromFile(screen, path, noReg)
     if noReg then
         local pal = assert(serialization.load(path))
-        for i = 0, 15 do
-            if graphic.getPaletteColor(screen, i) ~= pal[i + 1] then
-                graphic.setPaletteColor(screen, i, pal[i + 1])
-            end
-        end
+        graphic.setPalette(screen, pal)
     else
         pcall(sysinit.applyPalette, path, screen)
     end
