25c56129066c3bb65a97e296379390414f070bf8
igorkll <frerther@gmail.com>
2023-12-05 21:25:30 +0300

    Update sysinit.lua

--- system/lib/sysinit.lua
+++ system/lib/sysinit.lua
@@ -71,6 +71,12 @@ function sysinit.initScreen(screen)
         my = 25
     end
 
+    --вся эта хрень нужна для коректной инициализации чтобы экран не мигал при загрузке
+    gpu.setBackground(0x000000)
+    gpu.setForeground(0xffffff)
+    gpu.fill(1, 1, mx, my, " ")
+    graphic.forceUpdate(screen)
+
     graphic.setDepth(screen, 1)
     gpu.setBackground(0x000000)
     gpu.setForeground(0xffffff)
