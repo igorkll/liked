bf2af56a8d7099b0512dd60670ce90013d33da9a
igorkll <frerther@gmail.com>
2023-12-09 20:29:54 +0300

    Update recoveryScript.lua

--- system/recoveryScript.lua
+++ system/recoveryScript.lua
@@ -1,33 +1,13 @@
-if bootloader and bootloader.runlevel ~= "init" then
-    error("to start, the kernel does not need to be initialized, please restart the device", 0)
-end
+local component = component or require("component")
+local bootloader = bootloader or require("bootloader")
 
-local internet = component.proxy(component.list("internet")() or "")
-if not internet then
-    error("to start the installation, you need an Internet card", 0)
-end
-
-local function getInternetFile(url)
-    local handle, data, result, reason = internet.request(url), ""
-    if handle then
-        while true do
-            result, reason = handle.read(math.huge) 
-            if result then
-                data = data .. result
-            else
-                handle.close()
-                
-                if reason then
-                    return nil, reason
-                else
-                    return data
-                end
-            end
+recoveryApi.menu("Liked Recovery Tool", {
+    "Recovery From Internet"
+}, {
+    function ()
+        local internet = component.proxy(component.list("internet")() or "")
+        if internet then
+            local content = bootloader.readFile(bootloader.bootfs, "/system/likedlib/update/update.lua")
         end
-    else
-        return nil, "unvalid address"
     end
-end
-
-computer.getBootAddress = computer.tmpAddress --временный костыль шоб утихамирить кривой установшик
-assert(load(assert(getInternetFile("https://raw.githubusercontent.com/igorkll/likeOS/main/installer/maininstaller.lua"))))()
+})
