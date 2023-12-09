f9ceb8d0acef6300a82db09e01cce4fd4c76d849
igorkll <frerther@gmail.com>
2023-12-09 21:07:21 +0300

    Update recoveryScript.lua

--- system/recoveryScript.lua
+++ system/recoveryScript.lua
@@ -1,20 +1,49 @@
 local component = component or require("component")
 local bootloader = bootloader or require("bootloader")
+local computer = computer or require("computer")
 
-recoveryApi.menu("Liked Recovery Tool", {
-    "Recovery From Internet"
-}, {
-    function ()
-        if component.list("internet")() then
-            local sysdata = "{data={"
-            for _, file in ipairs(bootloader.bootfs.list("/system/sysdata")) do
-                local fullpath = "/system/sysdata/" .. file
-                sysdata = sysdata .. file .. "=" .. bootloader.readFile(fullpath) .. ","
-            end
-            sysdata = sysdata .. "}}"
-            local content = "local installdata = " .. sysdata .. "\n" .. bootloader.readFile(bootloader.bootfs, "/system/likedlib/update/update.lua")
-        else
-            recoveryApi.info("An internet card is required")
-        end 
-    end
-})
+local strs, funcs
+local startupPath = "/likeOS_startup.lua"
+
+if bootloader.bootfs.exists(startupPath) then
+    strs = {
+        "Cancel Scheduled Recover"
+    }
+
+    funcs = {
+        function ()
+            bootloader.bootfs.remove(startupPath)
+            computer.shutdown("fast")
+        end
+    }
+else
+    strs = {
+        "Recovery From Internet"
+    }
+
+    funcs = {
+        function ()
+            if component.list("internet")() then
+                local sysdata = {branch = "main", mode = "full"}
+                for _, file in ipairs(bootloader.bootfs.list("/system/sysdata") or {}) do
+                    sysdata[file] = bootloader.readFile(bootloader.bootfs, "/system/sysdata/" .. file)
+                end
+    
+                local sysdataStr = "{data={"
+                for key, data in pairs(sysdata) do
+                    sysdataStr = sysdataStr .. key .. "=\"" .. data .. "\","
+                end
+                sysdataStr = sysdataStr .. "}}"
+    
+                local content = "local installdata = " .. sysdataStr .. "\n" .. bootloader.readFile(bootloader.bootfs, "/system/likedlib/update/update.lua")
+                if bootloader.writeFile(bootloader.bootfs, startupPath, content) then
+                    computer.shutdown("fast")
+                end
+            else
+                recoveryApi.info("An internet card is required")
+            end 
+        end
+    }
+end
+
+recoveryApi.menu("Liked Recovery Tool", strs, funcs)
