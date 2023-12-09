f1991ed064221f7c1cd5e6d27b36b32e36d94ff8
igorkll <frerther@gmail.com>
2023-12-09 20:50:54 +0300

    Update recoveryScript.lua

--- system/recoveryScript.lua
+++ system/recoveryScript.lua
@@ -5,9 +5,16 @@ recoveryApi.menu("Liked Recovery Tool", {
     "Recovery From Internet"
 }, {
     function ()
-        local internet = component.proxy(component.list("internet")() or "")
-        if internet then
-            local content = bootloader.readFile(bootloader.bootfs, "/system/likedlib/update/update.lua")
-        end
+        if component.list("internet")() then
+            local sysdata = "{data={"
+            for _, file in ipairs(bootloader.bootfs.list("/system/sysdata")) do
+                local fullpath = "/system/sysdata/" .. file
+                sysdata = sysdata .. file .. "=" .. bootloader.readFile(fullpath) .. ","
+            end
+            sysdata = sysdata .. "}}"
+            local content = "local installdata = " .. sysdata .. "\n" .. bootloader.readFile(bootloader.bootfs, "/system/likedlib/update/update.lua")
+        else
+            recoveryApi.info("An internet card is required")
+        end 
     end
 })
