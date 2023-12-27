cdef0d3beec2a24e2334ad6d6a7988f4a1e90e95
igorkll <frerther@gmail.com>
2023-12-27 18:50:09 +0300

    Update main.lua

--- market/apps/midi.app/main.lua
+++ market/apps/midi.app/main.lua
@@ -5,7 +5,32 @@ local screen, nickname, path = ...
 local player = midi.create(path, midi.instruments())
 
 local ui = uix.manager(screen)
+local layout = ui:create("Midi Player")
+local playButton = layout:createButton(2, 2, 16, 1, nil, nil, "Play")
+local pauseButton = layout:createButton(2, 4, 16, 1, nil, nil, "Pause")
+local stopButton = layout:createButton(2, 6, 16, 1, nil, nil, "Stop")
 
+local playerThread
 
+function playButton:onClick()
+    if not playerThread then
+        playerThread = player.createThread(true)
+    end
+
+    playButton:resume()
+end
+
+function pauseButton:onClick()
+    if playerThread then
+        playButton:suspend()
+    end
+end
+
+function stopButton:onClick()
+    if playerThread then
+        playerThread:kill()
+        playerThread = nil
+    end
+end
 
 ui:loop()
