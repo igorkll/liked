5c86f8a1a8538fc57a6cc146cf309e1f5c2c8952
igorkll <frerther@gmail.com>
2024-01-01 14:29:44 +0300

    Update draw.lua

--- system/lib/draw.lua
+++ system/lib/draw.lua
@@ -1,11 +1,31 @@
 local graphic = require("graphic")
-local draw = {}
+local draw = {modes = {}}
+draw.modes.box = 0
+draw.modes.full = 1
+draw.modes.semi = 2
+draw.modes.braille = 3
+
+function draw:size()
+    local rx, ry = self.window.sizeX, self.window.sizeY
+    if self.mode == draw.modes.box then
+        return rx / 2, ry
+    elseif self.mode == draw.modes.full then
+        return rx, ry
+    elseif self.mode == draw.modes.semi then
+        return rx, ry * 2
+    elseif self.mode == draw.modes.braille then
+        return rx * 2, ry * 4
+    end
+end
 
 function draw:dot(x, y, color)
     x = math.round(x)
     y = math.round(y)
+    color = color or 0xffffff
 
-    if self.mode == "full" then
+    if self.mode == draw.modes.box then
+        self.window:set(((x - 1) * 2) + 1, y, color, 0, "  ")
+    elseif self.mode == draw.modes.full then
         self.window:set(x, y, color, 0, " ")
     end
 end
@@ -15,6 +35,7 @@ function draw:line(x0, y0, x1, y1, color)
     y0 = math.round(y0)
     x1 = math.round(x1)
     y1 = math.round(y1)
+    color = color or 0xffffff
 
     local sx, sy, e2, err;
     local dx = math.abs(x1 - x0);
@@ -32,14 +53,42 @@ function draw:line(x0, y0, x1, y1, color)
             err = err - dy; 
             x0 = x0 + sx; 
         end
-        if e2 < dx then 
+        if e2 < dx then
             err = err + dx
             y0 = y0 + sy
         end
     end
 end
 
+function draw:fill(x, y, sx, sy, color)
+    x = math.round(y)
+    x = math.round(y)
+    sx = math.round(sx)
+    sy = math.round(sy)
+    color = color or 0xffffff
+
+    if self.mode == draw.modes.box then
+        self.window:fill(x, y, sx * 2, sy, color, 0, " ")
+    elseif self.mode == draw.modes.full then
+        self.window:fill(x, y, sx, sy, color, 0, " ")
+    else
+        for ix = x, x + (sx - 1) do
+            for iy = y, y + (sy - 1) do
+                self:dot(ix, iy, color)
+            end
+        end
+    end
+end
+
+
+
+
 function draw.create(window, mode)
+    mode = mode or draw.modes.full
+    if mode < 0 or mode > draw.modes.braille then
+        error("the wrong mode", 2)
+    end
+
     if type(window) == "string" then
         window = graphic.create(window, 1, 1, graphic.getResolution(window))
     end
@@ -47,7 +96,7 @@ function draw.create(window, mode)
     return setmetatable(
         {
             window = window,
-            mode = mode or "full"
+            mode = mode
         },
         {
             __index = draw
