1d8ee53f9245c4f513d6ee9159d402466e2bc211
igorkll <frerther@gmail.com>
2024-01-03 00:05:31 +0300

    Update main.lua

--- system/bin/setup.app/main.lua
+++ system/bin/setup.app/main.lua
@@ -22,23 +22,32 @@ function blinckedHi:draw()
         color = uix.colors.gray
     elseif self.count == 3 then
         color = uix.colors.black
+    elseif self.count == 4 then
+        color = uix.colors.black
+    elseif self.count == 5 then
+        color = uix.colors.gray
+    elseif self.count == 6 then
+        color = uix.colors.lightGray
+    elseif self.count == 7 then
+        color = uix.colors.white
     end
 
-    self.gui.window:set(self.x, self.y + 0 , color, 0, "███                 ███")
-    self.gui.window:set(self.x, self.y + 1 , color, 0, "███                 ███")
-    self.gui.window:set(self.x, self.y + 2 , color, 0, "███                    ")
-    self.gui.window:set(self.x, self.y + 3 , color, 0, "█████████           ███")
-    self.gui.window:set(self.x, self.y + 4 , color, 0, "██████████          ███")
-    self.gui.window:set(self.x, self.y + 5 , color, 0, "███      ██         ███")
-    self.gui.window:set(self.x, self.y + 6 , color, 0, "███      ███        ███")
-    self.gui.window:set(self.x, self.y + 7 , color, 0, "███      ███        ███")
-    self.gui.window:set(self.x, self.y + 8 , color, 0, "███      ███        ███")
-    self.gui.window:set(self.x, self.y + 9 , color, 0, "███      ███        ███")
-    self.gui.window:set(self.x, self.y + 10, color, 0, "███      ███        ███")
-    self.gui.window:set(self.x, self.y + 11, color, 0, "███      ███        ███")
+    local bg = uix.colors.cyan
+    self.gui.window:set(self.x, self.y + 0 , bg, color, "███                 ███")
+    self.gui.window:set(self.x, self.y + 1 , bg, color, "███                 ███")
+    self.gui.window:set(self.x, self.y + 2 , bg, color, "███                    ")
+    self.gui.window:set(self.x, self.y + 3 , bg, color, "█████████           ███")
+    self.gui.window:set(self.x, self.y + 4 , bg, color, "██████████          ███")
+    self.gui.window:set(self.x, self.y + 5 , bg, color, "███      ██         ███")
+    self.gui.window:set(self.x, self.y + 6 , bg, color, "███      ███        ███")
+    self.gui.window:set(self.x, self.y + 7 , bg, color, "███      ███        ███")
+    self.gui.window:set(self.x, self.y + 8 , bg, color, "███      ███        ███")
+    self.gui.window:set(self.x, self.y + 9 , bg, color, "███      ███        ███")
+    self.gui.window:set(self.x, self.y + 10, bg, color, "███      ███        ███")
+    self.gui.window:set(self.x, self.y + 11, bg, color, "███      ███        ███")
 
     self.count = self.count + 1
-    if self.count >= 4 then
+    if self.count >= 8 then
         self.count = 0
     end
 end
@@ -48,7 +57,12 @@ end
 helloLayout = ui:simpleCreate(uix.colors.cyan, uix.styles[2])
 helloLayout:createText(2, 1, uix.colors.white, "liked & likeOS")
 
-hiObj = helloLayout:createCustom(rx / 2, ry / 2, blinckedHi)
+hiObj = helloLayout:createCustom((rx / 2) - 11, (ry / 2) - 6, blinckedHi)
+next1 = helloLayout:createButton((rx / 2) - 7, ry - 1, 16, 1, uix.colors.lightBlue, uix.colors.white, "next", true)
+
+function next1:onClick()
+    os.exit()
+end
 
 helloLayout:timer(0.2, function ()
     hiObj:draw()
