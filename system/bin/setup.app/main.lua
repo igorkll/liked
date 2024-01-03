7aaf1e3860a5f3aac89eadc38994e835316ad666
igorkll <frerther@gmail.com>
2024-01-03 14:37:24 +0300

    Update main.lua

--- system/bin/setup.app/main.lua
+++ system/bin/setup.app/main.lua
@@ -48,12 +48,20 @@ helloLayout:createText(2, 1, uix.colors.white, "liked & likeOS")
 hiObj = helloLayout:createCustom((rx / 2) - 11, (ry / 2) - 6, blinckedHi)
 hiObj:draw()
 
-local tick = 360    
-helloLayout:timer(0.1, function ()
-    local value = math.abs(math.sin(math.rad(tick)))
-    graphic.setPaletteColor(screen, colorlib.red, colorlib.blend(value * 255, value * 255, value * 255))
-    tick = tick + 8
-end, math.huge)
+local function blink()
+    local tick = 90
+    helloLayout:timer(0.1, function ()
+        local value = math.abs(math.sin(math.rad(tick)))
+        graphic.setPaletteColor(screen, colorlib.red, colorlib.blend(value * 255, value * 255, value * 255))
+        tick = (tick + 12) % 360
+
+        if tick > 180 + 90 then
+            return false
+        end
+    end, math.huge)
+end
+blink()
+helloLayout:timer(4, blink, math.huge)
 
 do
     local next1 = helloLayout:createButton((rx / 2) - 7, ry - 1, 16, 1, uix.colors.lightBlue, uix.colors.white, "next", true)
@@ -76,6 +84,12 @@ end
 
 licenseLayout = ui:simpleCreate(uix.colors.cyan, uix.styles[2])
 licenseLayout:createCustom(3, 2, gobjs.scrolltext, rx - 4, ry - 4, assert(fs.readFile("/system/LICENSE")):gsub("\r", ""))
+
+back1 = licenseLayout:createButton(3, ry - 1, 16, 1, uix.colors.lightBlue, uix.colors.white, " ← back", true)
+back1.alignment = "left"
+function back1:onClick()
+    ui:select(helloLayout)
+end
 
 --------------------------------
 
