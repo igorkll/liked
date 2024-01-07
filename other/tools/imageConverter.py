d6f0ecb5260a2798e6e6468b72cac2e1b47a87ac
igorkll <frerther@gmail.com>
2024-01-07 22:33:04 +0300

    backup

--- other/tools/imageConverter.py
+++ other/tools/imageConverter.py
@@ -166,6 +166,20 @@ def toCVcolor(color):
 def find_closest_palette_color(color):
     return toCVcolor(colors3t[find_closest_color(convert_to_24bit(color), colors3t)])
 
+def clamp(n, min, max): 
+    if n < min: 
+        return min
+    elif n > max: 
+        return max
+    else: 
+        return n 
+
+def colorDiff(c1, c2):
+    r, g, b = c1[2] / 255, c1[1] / 255, c1[0] / 255
+    r1, g1, b1 = c2[2] / 255, c2[1] / 255, c2[0] / 255
+    
+    return abs((r + g + b) - (r1 + g1 + b1)) / 3
+
 def floyd(image):
     height, width, channels = image.shape
 
@@ -173,16 +187,23 @@ def floyd(image):
         for x in range(width):
             print("currentpixel", image[y, x])
 
-            oldpixel = image[y, x]
+            oldpixel = [image[y, x][0], image[y, x][1], image[y, x][2], image[y, x][3]]
             newpixel = find_closest_palette_color(oldpixel)
             newpixel[3] = oldpixel[3]
             image[y, x] = newpixel
 
             print("currentpixel2", newpixel)
 
-            quant_error_b = abs(oldpixel[0] - newpixel[0]) / 255
-            quant_error_g = abs(oldpixel[1] - newpixel[1]) / 255
-            quant_error_r = abs(oldpixel[2] - newpixel[2]) / 255
+            # quant_error_b = (oldpixel[0] - newpixel[0]) / 1
+            # quant_error_g = (oldpixel[1] - newpixel[1]) / 1
+            # quant_error_r = (oldpixel[2] - newpixel[2]) / 1
+            quant_error = colorDiff(oldpixel, newpixel)
+            quant_error_b = quant_error
+            quant_error_g = quant_error
+            quant_error_r = quant_error
+
+            print("old data", oldpixel, oldpixel[0], oldpixel[1], oldpixel[2])
+            print("new data", newpixel, newpixel[0], newpixel[1], newpixel[2])
             print("add", quant_error_b, quant_error_g, quant_error_r)
             
             try:
@@ -214,6 +235,14 @@ def floyd(image):
                 print(f"colorset error 4: {e}")
 
             print("new color", image[y, x])
+
+    print("checking floyd")
+    for y in range(height):
+        for x in range(width):
+            pixel = image[y, x]
+            for i in range(4):
+                pixel[i] = round(clamp(pixel[i], 0, 255))
+            image[y, x] = pixel
 
 
 def parse_image_pixelwise(image_path):
