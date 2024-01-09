43e8603ef55de8203ee41da26fe24c16e390cb20
igorkll <frerther@gmail.com>
2024-01-10 00:11:18 +0300

    Update shell.lua

--- system/bin/shell.lua
+++ system/bin/shell.lua
@@ -21,7 +21,7 @@ local t = thread.create(function ()
     if not registry.systemConfigured then
         assert(apps.execute("setup", screen))
     end
-    if internet.check() and account.getLogin() and not account.checkToken() then
+    if internet.check() and account.getLocked() and not account.checkToken() then
         account.loginWindow(screen)
     end
     
