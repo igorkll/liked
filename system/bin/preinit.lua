21908e3f0fc785835e24dda47938d3194c026e50
igorkll <frerther@gmail.com>
2024-01-10 22:35:01 +0300

    Update preinit.lua

--- system/bin/preinit.lua
+++ system/bin/preinit.lua
@@ -3,26 +3,29 @@ local fs = require("filesystem")
 local event = require("event")
 local programs = require("programs")
 local internet = require("internet")
+local thread = require("thread")
 
 local storagePath = "/data/userdata/cloudStorage"
 
 local function check()
-    if internet.check() then
-        account.check()
-        
-        local storage = account.getStorage()
-        if storage then
-            if not fs.exists(storagePath) then
-                fs.mount(storage, storagePath)
+    thread.createBackground(function ()
+        if internet.check() then
+            account.check()
+            
+            local storage = account.getStorage()
+            if storage then
+                if not fs.exists(storagePath) then
+                    fs.mount(storage, storagePath)
+                end
+            elseif fs.exists(storagePath) then
+                fs.umount(storagePath)
             end
-        elseif fs.exists(storagePath) then
-            fs.umount(storagePath)
-        end
 
-        if account.isBricked() then
-            assert(programs.execute("/system/liked/brick.lua"))
+            if account.isBricked() then
+                assert(programs.execute("/system/liked/brick.lua"))
+            end
         end
-    end
+    end)
 end
 
 event.timer(5, check, math.huge)
