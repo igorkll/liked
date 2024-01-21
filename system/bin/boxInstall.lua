5aaacc6a05e86771db4bc25ee6ee46bd4bb03f0f
igorkll <frerther@gmail.com>
2024-01-21 14:24:43 +0300

    box icons added

--- system/bin/boxInstall.lua
+++ system/bin/boxInstall.lua
@@ -6,7 +6,9 @@ local clear = gui.saveBigZone(screen)
 local vfs = gui.selectExternalFs(screen)
 if not vfs then return end
 clear()
-local ok, err = installer.ui_install_boxfile(screen, vfs, path)
-if not ok then
-    gui.bigWarn(screen, nil, nil, err)
+if gui.yesno(screen, nil, nil, "are you sure you want to flash this file to the \"" .. require("paths").name(require("hdd").genName(vfs.address)) .. "\" disk? THE DISK WILL BE CLEARED") then
+    local ok, err = installer.ui_install_boxfile(screen, vfs, path)
+    if not ok then
+        gui.bigWarn(screen, nil, nil, err)
+    end
 end
