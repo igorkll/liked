2e850718add29195a5623f4b3fd52c840344200a
igorkll <frerther@gmail.com>
2024-01-22 23:50:36 +0300

    Update liked.lua

--- system/lib/liked.lua
+++ system/lib/liked.lua
@@ -733,18 +733,6 @@ function liked.getComputerScore()
         score = score + 1
     end
 
-    if ram >= 2048 then
-        score = score + 5
-    elseif ram >= 1024 + 512 then
-        score = score + 4
-    elseif ram >= 1024 then
-        score = score + 3
-    elseif ram >= 768 then
-        score = score + 2
-    else
-        score = score + 1
-    end
-
     return score
 end
 
