35a735730d87357c4f0e3250725cf5ddf96c0c9f
igorkll <frerther@gmail.com>
2024-01-23 22:48:53 +0300

    Update stub.lua

--- system/bin/setup.app/stub.lua
+++ system/bin/setup.app/stub.lua
@@ -1,7 +1,7 @@
 local graphic = require("graphic")
-local gui_contrainer = require("gui_contrainer")
+local gui_container = require("gui_container")
 local screen = ...
 
-graphic.clear(screen, gui_contrainer.colors.cyan)
-graphic.set(screen, 2, 2, gui_contrainer.colors.cyan, gui_contrainer.colors.white, "configuration expectations...")
+graphic.clear(screen, gui_container.colors.cyan)
+graphic.set(screen, 2, 2, gui_container.colors.cyan, gui_container.colors.white, "configuration expectations...")
 graphic.forceUpdate(screen)
