f8d2ccdb7ccf19c6cf01d02549c053ed4459645b
igorkll <frerther@gmail.com>
2024-02-12 15:25:05 +0300

    Update main.lua

--- market/apps/bootmanager.app/files/main.lua
+++ market/apps/bootmanager.app/files/main.lua
@@ -1,3 +1,21 @@
-local function getGPU()
-    
+local function getGPU(screen)
+    for address in component.list("gpu", true) do
+        if component.invoke(address, "getScreen") == screen then
+            return component.proxy(address)
+        end
+    end
+    local gpu = component.proxy(component.list("gpu", true)() or "")
+    if gpu then
+        gpu.bind(screen, true)
+        return gpu
+    end
 end
+
+local function screens()
+    local tbl = {}
+    for address in component.list("gpu", true) do
+        tbl[address] = getGPU(address)
+    end
+    return pairs(tbl)
+end
+
