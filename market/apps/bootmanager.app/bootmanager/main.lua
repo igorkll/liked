8c744b086bb366740e73c6d4ff48429437824e87
igorkll <frerther@gmail.com>
2024-02-12 17:18:06 +0300

    Update main.lua

--- market/apps/bootmanager.app/bootmanager/main.lua
+++ market/apps/bootmanager.app/bootmanager/main.lua
@@ -6,7 +6,7 @@ local function getGPU(screen)
     end
     local gpu = component.proxy(component.list("gpu", true)() or "")
     if gpu then
-        gpu.bind(screen, true)
+        gpu.bind(screen, false)
         return gpu
     end
 end
@@ -30,18 +30,63 @@ local function centerPrint(gpu, y, text)
     gpu.set(((rx / 2) - (unicode.len(text) / 2)) + 1, y, text)
 end
 
-local function menu(label, points, funcs)
-    for gpu in screens() do
-        local rx, ry = gpu.getResolution()
-        
-        invert(gpu)
-        gpu.fill(1, 1, rx, 1, " ")
-        centerPrint(gpu, 1, label)
-        invert(gpu)
+local function menu(label, strs, funcs, autoTimeout)
+    local selected = 1
+    local startTime = computer.uptime()
+
+    local function redraw(otherTime)
+        for gpu in screens() do
+            local rx, ry = gpu.getResolution()
+            gpu.fill(1, 1, rx, ry, " ")
+
+            invert(gpu)
+
+            gpu.fill(1, 1, rx, 1, " ")
+            centerPrint(gpu, 1, label)
+
+            gpu.fill(1, ry, rx, 1, " ")
+            gpu.set(2, ry, "Enter=Choose  ↑-UP  ↓-DOWN")
+
+            invert(gpu)
+
+            gpu.set(2, ry - 3, "autorun of the selected system after: " .. math.floor(otherTime or (autoTimeout - (computer.uptime() - selected))))
+        end
     end
+    redraw()
 
     while true do
-        local eventData = {computer.pullSignal()}
+        local eventData = {computer.pullSignal(autoTimeout and 0.5)}
+        if eventData[1] == "key_down" then
+            if eventData[4] == 28 then
+                if funcs[selected](strs[selected], eventData[5]) then
+                    break
+                end
+            elseif eventData[4] == 200 then
+                autoTimeout = nil
+                selected = selected - 1
+                if selected < 1 then
+                    selected = 1
+                else 
+                    redraw()
+                end
+            elseif eventData[4] == 208 then
+                autoTimeout = nil
+                selected = selected + 1
+                if selected > #strs then
+                    selected = #strs
+                else
+                    redraw()
+                end
+            end
+        end
+
+        if autoTimeout then
+            if autoTimeout - (computer.uptime() - selected) <= 0 then
+                redraw(0)
+                return 1
+            end
+            redraw()
+        end
     end
 end
 
@@ -59,7 +104,7 @@ for gpu in screens() do
     gpu.fill(1, 1, mx, my, " ")
 end
 
-menu("Like Boot-Manager", {
-    "one",
-    "two"
-})
+local strs = {}
+local funcs = {}
+
+menu("Like Boot-Manager", strs, funcs, 3)
