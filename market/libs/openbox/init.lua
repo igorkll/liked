6f292358a982c19c34b130a666194aa47e99b3ba
igorkll <frerther@gmail.com>
2024-02-14 17:38:40 +0300

    Update init.lua

--- market/libs/openbox/init.lua
+++ market/libs/openbox/init.lua
@@ -1,16 +1,22 @@
 local vmx = require("vmx")
 local system = require("system")
 local graphic = require("graphic")
+local paths = require("paths")
+local fs = require("filesystem")
 
 local boxPath = system.getResourcePath("box")
 local eepromPath = system.getResourcePath("eepromImage")
-
 local openbox = {}
 
-function openbox.create(screen)
+function openbox.run(screen, program)
     local vm = vmx.create(eepromPath)
-    vm.bindComponent()
+    local gpuAddress = graphic.findGpuAddress(screen)
+    fs.copy(program, paths.concat(boxPath, "autorun.lua"))
+    graphic.gpuPrivateList[gpuAddress] = true
+    vm.bindComponent(vmx.getComponent(gpuAddress))
+    vm.bindComponent(vmx.getComponent(screen))
     vm.loop()
+    graphic.gpuPrivateList[gpuAddress] = nil
 end
 
 openbox.unloadable = true
