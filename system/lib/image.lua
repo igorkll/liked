616374424625f1bb15ad0207488a18af9cd5d246
igorkll <frerther@gmail.com>
2024-05-28 19:50:13 +0300

    small ui fix
    "_" > " " in screensaver and wallpaper

--- system/lib/image.lua
+++ system/lib/image.lua
@@ -58,8 +58,8 @@ function image.draw(screen, path, x, y, wallpaperMode, forceFullColor, lightMul,
         llcolors = image.readPalette(path, false, screen) or colors
     end
 
-    x = math.floor(x)
-    y = math.floor(y)
+    x = math.floor(x + 0.5)
+    y = math.floor(y + 0.5)
     
     lightMul = lightMul or 1
     path = repath(screen, path)
