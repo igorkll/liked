0d12a5191aa986029a72b06bc837a0ef15ea8233
igorkll <frerther@gmail.com>
2024-06-19 21:57:55 +0300

    Update main.lua

--- market/apps/controller.app/main.lua
+++ market/apps/controller.app/main.lua
@@ -4,10 +4,12 @@ local screen = ...
 local ui = uix.manager(screen)
 local rx, ry = ui:zoneSize()
 
-local infoLayout = ui:create("controller [INFO]", uix.colors.black)
-infoLayout:createText(1, ry, uix.colors.white, "to use, flash the EEPROM of the robot/drone with the \"RC Control\" firmware through the settings>eeprom")
+----------------------------- ui
 
-local layout = ui:create("controller [RC Control]", uix.colors.black)
+layout = ui:create("controller [RC Control]", uix.colors.black)
+
+infoLayout = ui:create("controller [INFO]", uix.colors.black)
+infoLayout:createText(1, ry, uix.colors.white, "to use, flash the EEPROM of the robot/drone with the \"RC Control\" firmware through the settings>eeprom")
 layout:setReturnLayout(infoLayout, uix.colors.green, " INFO ")
 
 ui:loop()
