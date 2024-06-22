dd6c2bec41aee6bbc35e44f81da8f6504f63f787
igorkll <frerther@gmail.com>
2024-06-22 10:26:32 +0300

    Update code.lua

--- market/apps/controller.app/firmware/rc_control/code.lua
+++ market/apps/controller.app/firmware/rc_control/code.lua
@@ -1,1 +1,20 @@
-local 
+local modem
+for address in component.list("modem", true) do
+    if component.invoke(address, "isWireless") then
+        if component.invoke(address, "setStrength", math.huge) >= 400 then
+            modem = address
+            break
+        end
+    end
+end
+if not modem then
+    modem = component.list("modem", true)()
+    if not modem then
+        error("the modem was not found", 0)
+    end
+end
+modem = component.proxy(modem)
+
+local port = 38710
+modem.close()
+modem.open(port)
