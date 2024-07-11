8e0e954107e05b9815a9c10bba277a3e6b66892b
igorkll <frerther@gmail.com>
2024-07-11 13:55:42 +0300

    backup

--- system/firmware/restricted_loader/code.lua
+++ system/firmware/restricted_loader/code.lua
@@ -1,4 +1,4 @@
-_likedLoader = true
+_restrictedLoader = true
 local invoke = component.invoke
 local eeprom = component.proxy(component.list("eeprom")())
 local func = load("return " .. eeprom.getData(), nil, nil, {})
@@ -39,26 +39,18 @@ end
 
 local addr = data.a or ""
 
-if not component.proxy(addr) then
-    for address in component.list("filesystem") do
-        if checkSystem(address) then
-            addr = address
-            computer.setBootAddress(addr)
-            break
-        end
-    end
-end
-
 if not checkSystem(addr) then
     addr = ""
 end
 
 if not component.proxy(addr) then
-    error("liked loader: could not find a suitable OS to boot", 0)
+    error("restricted loader: could not find a suitable OS to boot", 0)
 end
 
 invoke(computer.tmpAddress(), "makeDirectory", "bootloader") --blocks bootmanager startup
-invoke(addr, "remove", "bootloader") --attempt to remove bootmanager. liked loader runs only the verified operating systems
+invoke(addr, "remove", "bootloader") --attempt to remove bootmanager. restricted loader runs only the verified operating systems
+invoke(addr, "remove", "/vendor/apps/bootmanager.app")
+
 local file = invoke(addr, "open", "/init.lua")
 local buffer = ""
 repeat
---
+++ system/firmware/restricted_loader/data.lua
@@ -0,0 +1,31 @@
+local screen, _, hidden = ...
+
+if hidden then
+    return
+end
+
+local gui = require("gui")
+local fs = require("filesystem")
+local serialization = require("serialization")
+
+if not gui.nextOrCancel(screen, nil, nil, "Attention 1!!! after installation, you will not be able to boot into other operating systems") then
+    return true
+end
+
+if not gui.nextOrCancel(screen, nil, nil, "Attention 2!!! this loader verifies the authenticity of your OS for changes, it will not load the OS if changes are made to it") then
+    return true
+end
+
+if not gui.nextOrCancel(screen, nil, nil, "Attention 3!!! with this loader, you will not be able to use bootmanager. bootmanager will simply be deleted from the computer if it has been installed") then
+    return true
+end
+
+if not gui.nextOrCancel(screen, nil, nil, "Attention 4!!! this loader will not allow you to boot from other disks except from where it was installed") then
+    return true
+end
+
+if not gui.nextOrCancel(screen, nil, nil, "this thing is needed for extremely specific tasks, DO NOT INSTALL IT IF YOU DO NOT KNOW WHY YOU NEED IT") then
+    return true
+end
+
+return serialization.serialize({a = fs.bootmanager})
