d306dd460cc90d528bccb02ce9d5c1408ecceeb1
igorkll <frerther@gmail.com>
2025-02-19 15:02:46 +0300

    Update lua5_2.lua

--- market/apps/openOS.app/lua5_2.lua
+++ market/apps/openOS.app/lua5_2.lua
@@ -1,2 +1,10 @@
-saveFile("/tmp/bootTo", "/openOS.lua")
-require("computer").setArchitecture("Lua 5.2")
+local screen = ...
+local component = require("component")
+local fs = require("filesystem")
+
+fs.writeFile("/tmp/bootloader/bootfile", "/openOS.lua")
+require("bootloader").initScreen(component.gpu, screen)
+
+local computer = require("natives").computer
+computer.setArchitecture("Lua 5.2")
+computer.shutdown("fast")
